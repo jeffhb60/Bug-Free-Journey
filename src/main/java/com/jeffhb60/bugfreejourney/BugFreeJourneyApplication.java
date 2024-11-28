001a04db06c75a35f976c15876cab4f0fd71301c
Jeff Butt <jbutt2@wgu.edu>
2024-11-28 16:33:22 -0500

    Initial Commit.  Spring Boot Project.

---
+++ src/main/java/com/jeffhb60/bugfreejourney/BugFreeJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.bugfreejourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugFreeJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugFreeJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffhb60/bugfreejourney/BugFreeJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.bugfreejourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugFreeJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
